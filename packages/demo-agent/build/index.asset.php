c3084814ed7b429922bd90e0949be90217b3e205
Adan0423 <atrinidad.a4@gmail.com>
2026-01-03 18:06:55 -0500

    build(demo-agent): Add compiled JavaScript assets and their dependencies.

--- packages/demo-agent/build/index.asset.php
+++ packages/demo-agent/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-components', 'wp-element'), 'version' => '07704343194b99bf74ee');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-components', 'wp-element'), 'version' => 'b17a56299298909081f5');
